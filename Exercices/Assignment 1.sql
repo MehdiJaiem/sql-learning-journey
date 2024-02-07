6842e543e94325c313f5f0008bf928b189ab5ada
Spearitch <mehdijaiemkit1@gmail.com>
2024-02-07 11:14:17 +0100

    nothing special

--- Exercices/Assignment 1.sql
+++ Exercices/Assignment 1.sql
@@ -27,4 +27,36 @@ VALUES
     (NULL, 'Finance');
 
 -- Write a query to find all employees in a specific department, e.g., 'Engineering'.
-SELECT name FROM employees_2 WHERE department_id=1;
+SELECT * FROM employees_2 WHERE department_id = 
+(SELECT department_id FROM departments_2 WHERE department_name = 'Engineering');
+
+-- Counting all number of employees
+SELECT COUNT(*) FROM employees_2;
+
+-- Write a query to list all employees, sorted by their salary in descending order.
+SELECT name FROM employees_2 ORDER BY salary DESC;
+
+-- Select one employee and give them a raise. Update their salary in the employees table.
+UPDATE employees_2 SET salary = salary + 5000 WHERE name = 'Bubu';
+SELECT * FROM employees_2 WHERE name="Bubu";
+-- Delete an employee record from the employees table (choose an employee_id that exists in your table).
+DELETE FROM employees_2 WHERE employee_id = 1;
+SELECT * FROM employees_2;
+
+-- Insert at least three new records into the employees table in a single query. Try to include a variety of positions and salaries.
+INSERT INTO employees_2
+VALUES(
+	(NULL, 'Moncef', 'Knatri', 69, '1000-01-25',1),
+    (NULL, 'Walid', 'Banzarti', 1, '2000-01-25',3),
+    (NULL, 'Wassim', 'Pepe Onzima', 0, '2020-04-16')
+);
+-- READ AND SORT
+-- Retrieve all employees, but sort them by their hired_date in ascending order.
+SELECT * FROM employees_2 ORDER BY hired_date DESC;
+
+-- Retrieve only the names and positions of employees, sorted by salary in descending order.
+SELECT name, position FROM employees_2 ORDER BY salary DESC;
+
+-- FILTERING DATA
+SELECT * from employees_2 WHERE salary > 55000;
+SELECT * FROM employees_2 WHERE YEAR(hired_date)=2021;
